9b78819e6aa5544219bfeeb4a44bdb776709b3ae
Logan Riggs <logan.riggs@dremio.com>
2023-05-02 14:38:26 -0700

    Recreate C++ library fix change.

--- ci/scripts/java_jni_manylinux_aarch64_build.sh
+++ ci/scripts/java_jni_manylinux_aarch64_build.sh
@@ -21,36 +21,36 @@ set -ex
 
 arrow_dir=${1}
 build_dir=${2}
-normalized_arch=$(arch)
-case ${normalized_arch} in
-  arm64)
-    normalized_arch=aarch_64
-    ;;
-  i386)
-    normalized_arch=x86_64
-    ;;
-esac
 # The directory where the final binaries will be stored when scripts finish
-dist_dir=${3}/${normalized_arch}
+dist_dir=${3}/$(arch)
 
 echo "=== Clear output directories and leftovers ==="
 # Clear output directories and leftovers
 rm -rf ${build_dir}
 
 echo "=== Building Arrow C++ libraries ==="
-: ${ARROW_BUILD_TESTS:=OFF}
+devtoolset_version=$(rpm -qa "devtoolset-*-gcc" --queryformat %{VERSION} | \
+                       grep -o "^[0-9]*")
+devtoolset_include_cpp="/opt/rh/devtoolset-${devtoolset_version}/root/usr/include/c++/${devtoolset_version}"
 : ${ARROW_DATASET:=ON}
-: ${ARROW_FILESYSTEM:=ON}
-: ${ARROW_GANDIVA_JAVA:=ON}
 : ${ARROW_GANDIVA:=ON}
+: ${ARROW_GANDIVA_JAVA:=ON}
+: ${ARROW_FILESYSTEM:=ON}
+: ${ARROW_JEMALLOC:=ON}
+: ${ARROW_RPATH_ORIGIN:=ON}
 : ${ARROW_ORC:=ON}
 : ${ARROW_PARQUET:=ON}
-: ${ARROW_PLASMA_JAVA_CLIENT:=ON}
 : ${ARROW_PLASMA:=ON}
+: ${ARROW_PLASMA_JAVA_CLIENT:=ON}
 : ${ARROW_PYTHON:=OFF}
 : ${ARROW_S3:=ON}
+: ${ARROW_BUILD_TESTS:=OFF}
 : ${CMAKE_BUILD_TYPE:=Release}
 : ${CMAKE_UNITY_BUILD:=ON}
+: ${VCPKG_ROOT:=/opt/vcpkg}
+: ${VCPKG_FEATURE_FLAGS:=-manifests}
+: ${VCPKG_TARGET_TRIPLET:=${VCPKG_DEFAULT_TRIPLET:-x64-linux-static-${CMAKE_BUILD_TYPE}}}
+: ${GANDIVA_CXX_FLAGS:=-isystem;${devtoolset_include_cpp};-isystem;${devtoolset_include_cpp}/aarch64-redhat-linux;-isystem;-lpthread}
 
 export ARROW_TEST_DATA="${arrow_dir}/testing/data"
 export PARQUET_TEST_DATA="${arrow_dir}/cpp/submodules/parquet-testing/data"
@@ -62,78 +62,72 @@ pushd "${build_dir}/cpp"
 cmake \
   -DARROW_BOOST_USE_SHARED=OFF \
   -DARROW_BROTLI_USE_SHARED=OFF \
+  -DARROW_BUILD_SHARED=ON \
   -DARROW_BUILD_TESTS=${ARROW_BUILD_TESTS} \
   -DARROW_BUILD_UTILITIES=OFF \
   -DARROW_BZ2_USE_SHARED=OFF \
   -DARROW_DATASET=${ARROW_DATASET} \
+  -DARROW_DEPENDENCY_SOURCE="VCPKG" \
   -DARROW_FILESYSTEM=${ARROW_FILESYSTEM} \
-  -DARROW_GANDIVA=${ARROW_GANDIVA} \
   -DARROW_GANDIVA_JAVA=${ARROW_GANDIVA_JAVA} \
-  -DARROW_GANDIVA_STATIC_LIBSTDCPP=ON \
-  -DARROW_GFLAGS_USE_SHARED=OFF \
+  -DARROW_GANDIVA_PC_CXX_FLAGS=${GANDIVA_CXX_FLAGS} \
+  -DARROW_GANDIVA=${ARROW_GANDIVA} \
   -DARROW_GRPC_USE_SHARED=OFF \
+  -DARROW_JEMALLOC=${ARROW_JEMALLOC} \
   -DARROW_JNI=ON \
   -DARROW_LZ4_USE_SHARED=OFF \
   -DARROW_OPENSSL_USE_SHARED=OFF \
   -DARROW_ORC=${ARROW_ORC} \
   -DARROW_PARQUET=${ARROW_PARQUET} \
-  -DARROW_PLASMA=${ARROW_PLASMA} \
   -DARROW_PLASMA_JAVA_CLIENT=${ARROW_PLASMA_JAVA_CLIENT} \
+  -DARROW_PLASMA=${ARROW_PLASMA} \
   -DARROW_PROTOBUF_USE_SHARED=OFF \
   -DARROW_PYTHON=${ARROW_PYTHON} \
+  -DARROW_RPATH_ORIGIN=${ARROW_RPATH_ORIGIN} \
   -DARROW_S3=${ARROW_S3} \
   -DARROW_SNAPPY_USE_SHARED=OFF \
   -DARROW_THRIFT_USE_SHARED=OFF \
   -DARROW_UTF8PROC_USE_SHARED=OFF \
   -DARROW_ZSTD_USE_SHARED=OFF \
-  -DAWSSDK_SOURCE=BUNDLED \
   -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} \
   -DCMAKE_INSTALL_LIBDIR=lib \
   -DCMAKE_INSTALL_PREFIX=${build_dir}/cpp \
   -DCMAKE_UNITY_BUILD=${CMAKE_UNITY_BUILD} \
+  -DORC_SOURCE=BUNDLED \
+  -DORC_PROTOBUF_EXECUTABLE=${VCPKG_ROOT}/installed/${VCPKG_TARGET_TRIPLET}/tools/protobuf/protoc \
   -DPARQUET_BUILD_EXAMPLES=OFF \
   -DPARQUET_BUILD_EXECUTABLES=OFF \
   -DPARQUET_REQUIRE_ENCRYPTION=OFF \
-  -Dre2_SOURCE=BUNDLED \
+  -DPythonInterp_FIND_VERSION_MAJOR=3 \
+  -DPythonInterp_FIND_VERSION=ON \
+  -DVCPKG_MANIFEST_MODE=OFF \
+  -DVCPKG_TARGET_TRIPLET=${VCPKG_TARGET_TRIPLET} \
   -GNinja \
   ${arrow_dir}/cpp
-cmake --build . --target install
+ninja install
 
-if [ "${ARROW_BUILD_TESTS}" == "ON" ]; then
-  ctest
+if [ $ARROW_BUILD_TESTS = "ON" ]; then
+  ctest \
+    --label-regex unittest \
+    --output-on-failure \
+    --parallel $(nproc) \
+    --timeout 300
 fi
 
 popd
 
 
-${arrow_dir}/ci/scripts/java_jni_build.sh \
+JAVA_JNI_CMAKE_ARGS=""
+JAVA_JNI_CMAKE_ARGS="${JAVA_JNI_CMAKE_ARGS} -DVCPKG_MANIFEST_MODE=OFF"
+JAVA_JNI_CMAKE_ARGS="${JAVA_JNI_CMAKE_ARGS} -DVCPKG_TARGET_TRIPLET=${VCPKG_TARGET_TRIPLET}"
+export JAVA_JNI_CMAKE_ARGS
+bash ${arrow_dir}/ci/scripts/java_jni_build.sh \
   ${arrow_dir} \
   ${build_dir} \
   ${dist_dir}
 
 
 echo "=== Copying libraries to the distribution folder ==="
-mkdir -p "${dist_dir}"
-cp -L ${build_dir}/cpp/lib/libgandiva_jni.dylib ${dist_dir}
-cp -L ${build_dir}/cpp/lib/libarrow_dataset_jni.dylib ${dist_dir}
-cp -L ${build_dir}/cpp/lib/libarrow_orc_jni.dylib ${dist_dir}
-
-echo "=== Checking shared dependencies for libraries ==="
-
-pushd ${dist_dir}
-archery linking check-dependencies \
-  --allow CoreFoundation \
-  --allow libSystem \
-  --allow libarrow_cdata_jni \
-  --allow libarrow_dataset_jni \
-  --allow libarrow_orc_jni \
-  --allow libc++ \
-  --allow libcurl \
-  --allow libgandiva_jni \
-  --allow libncurses \
-  --allow libz \
-  libarrow_cdata_jni.dylib \
-  libarrow_dataset_jni.dylib \
-  libarrow_orc_jni.dylib \
-  libgandiva_jni.dylib
-popd
+cp -L ${build_dir}/cpp/lib/libgandiva_jni.so ${dist_dir}
+cp -L ${build_dir}/cpp/lib/libarrow_dataset_jni.so ${dist_dir}
+cp -L ${build_dir}/cpp/lib/libarrow_orc_jni.so ${dist_dir}
