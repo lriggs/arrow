851449d8d754ddc049e425fd9663e51d69653de3
Logan Riggs <logan.riggs@dremio.com>
2024-09-06 11:41:54 -0700

    Remove unused import

--- java/gandiva/src/main/java/org/apache/arrow/gandiva/evaluator/ExpressionRegistry.java
+++ java/gandiva/src/main/java/org/apache/arrow/gandiva/evaluator/ExpressionRegistry.java
@@ -16,7 +16,6 @@
  */
 package org.apache.arrow.gandiva.evaluator;
 
-import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.google.protobuf.InvalidProtocolBufferException;
 import java.util.List;
