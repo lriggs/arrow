d4a8f384659448319e97e9681bd82224b05812b6
Logan Riggs <logan.riggs@dremio.com>
2025-03-25 18:30:46 -0700

    Try some optimization passes.

--- cpp/src/gandiva/engine.cc
+++ cpp/src/gandiva/engine.cc
@@ -73,8 +73,11 @@
 #  include <llvm/IR/PassManager.h>
 #  include <llvm/MC/TargetRegistry.h>
 #  include <llvm/Passes/PassPlugin.h>
+#  include "llvm/Transforms/IPO/ConstantMerge.h"
 #  include <llvm/Transforms/IPO/GlobalOpt.h>
+#  include "llvm/Transforms/Scalar/ADCE.h"
 #  include <llvm/Transforms/Scalar/NewGVN.h>
+#  include "llvm/Transforms/Scalar/SCCP.h"
 #  include <llvm/Transforms/Scalar/SimplifyCFG.h>
 #  include <llvm/Transforms/Utils/Mem2Reg.h>
 #  include <llvm/Transforms/Vectorize/LoopVectorize.h>
@@ -458,7 +461,8 @@ static void OptimizeModuleWithNewPassManager(llvm::Module& module,
   pass_builder.registerPipelineStartEPCallback([&](llvm::ModulePassManager& module_pm,
                                                    llvm::OptimizationLevel Level) {
     module_pm.addPass(llvm::ModuleInlinerPass());
-
+    module_pm.addPass(llvm::ConstantMergePass());
+    
     llvm::FunctionPassManager function_pm;
     function_pm.addPass(llvm::InstCombinePass());
     function_pm.addPass(llvm::PromotePass());
@@ -467,6 +471,9 @@ static void OptimizeModuleWithNewPassManager(llvm::Module& module,
     function_pm.addPass(llvm::SimplifyCFGPass());
     function_pm.addPass(llvm::LoopVectorizePass());
     function_pm.addPass(llvm::SLPVectorizerPass());
+
+    function_pm.addPass(llvm::SCCPPass());
+    function_pm.addPass(llvm::ADCEPass());
     module_pm.addPass(llvm::createModuleToFunctionPassAdaptor(std::move(function_pm)));
 
     module_pm.addPass(llvm::GlobalOptPass());
