b1c9fe396676d5bbe32a6fed6cf2dd7a70d862ef
Logan Riggs <logan.riggs@dremio.com>
2025-09-17 14:08:52 -0700

    Rename module for IR

--- cpp/src/gandiva/engine.cc
+++ cpp/src/gandiva/engine.cc
@@ -314,7 +314,8 @@ Engine::Engine(const std::shared_ptr<Configuration>& conf,
       target_machine_(std::move(target_machine)),
       conf_(conf) {
   // LLVM 10 doesn't like the expr function name to be the same as the module name
-  auto module_id = "gdv_module_" + std::to_string(reinterpret_cast<uintptr_t>(this));
+  // auto module_id = "gdv_module_" + std::to_string(reinterpret_cast<uintptr_t>(this));
+  auto module_id = "codegen";
   module_ = std::make_unique<llvm::Module>(module_id, *context_);
   module_->setDataLayout(target_machine_->createDataLayout());
 }
